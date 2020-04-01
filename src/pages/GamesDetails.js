caeac80f4a01db0caf1938745914689bd2050825
Van <vango85@gmail.com>
2020-04-01 10:55:53 +0200

    [xx] Updates after live session 2

--- src/pages/GamesDetails.js
+++ src/pages/GamesDetails.js
@@ -1,38 +1,50 @@
 /* eslint-disable indent */
-import React, {useEffect, useState} from 'react';
-import {useParams, Link} from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useParams, Link, useHistory } from "react-router-dom";
 
 export const GamesDetails = () => {
-    const {slug} = useParams();
-    const GAMES_URL = `https://api.rawg.io/api/games/${slug}`;
-    const [game, setGame] = useState();
+  const { slug } = useParams();
+  const GAMES_URL = `https://api.rawg.io/api/games/${slug}`;
+  const history = useHistory();
+  const [game, setGame] = useState();
+  const [statusCode, setStatusCode] = useState(200);
 
-    useEffect(() => {
-        fetch(GAMES_URL)
-            .then(res => res.json())
-            .then(json => setGame(json))
-    }, [GAMES_URL, slug]);
+  useEffect(() => {
+    fetch(GAMES_URL)
+      .then(res => {
+        setStatusCode(res.status);
+        return res.json();
+      })
+      .then(json => setGame(json));
+  }, [GAMES_URL, slug]);
 
-    if (!game) {
-        return <></>;
+  useEffect(() => {
+    console.log(`Status code: ${statusCode}`);
+    if (statusCode !== 200) {
+      // Do something here to avoid showing an error to the user
+      history.push("/games");
     }
+  }, [history, statusCode]);
 
-    return (
-        <section className="game-details">
-            <h2>{game.name}</h2>
-            <section className="game-overview">
-                <img src={game.background_image} alt={game.name} />
-                <section className="game-stats">
-                    <h3>Rating: {game.rating}</h3>
-                    <h3>Genre: {game.genres.map(genre => {
-                        return (
-                            <Link to={`/genres/${genre.id}`}>{genre.name} </Link>
-                        )
-                    })}
-                    </h3>
+  if (!game) {
+    return <></>;
+  }
 
-                </section>
-            </section>
+  return (
+    <section className="game-details">
+      <h2>{game.name}</h2>
+      <section className="game-overview">
+        <img src={game.background_image} alt={game.name} />
+        <section className="game-stats">
+          <h3>Rating: {game.rating}</h3>
+          <h3>
+            Genre:{" "}
+            {game.genres.map(genre => {
+              return <Link to={`/genres/${genre.id}`}>{genre.name} </Link>;
+            })}
+          </h3>
         </section>
-    )
-}
+      </section>
+    </section>
+  );
+};
--- src/pages/GenreDetails.js
+++ src/pages/GenreDetails.js
@@ -1,34 +1,35 @@
 /* eslint-disable indent */
-import React, {useEffect, useState} from 'react';
-import {useParams} from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useParams, useHistory } from "react-router-dom";
 
 export const GenreDetails = () => {
-    const {id} = useParams();
-    const GENRE_URL = `https://api.rawg.io/api/genres/${id}`;
-    const [genre, setGenre] = useState();
+  const { id } = useParams();
+  const history = useHistory();
+  const GENRE_URL = `https://api.rawg.io/api/genres/${id}`;
+  const [genre, setGenre] = useState();
 
-    useEffect(() => {
-        fetch(GENRE_URL)
-            .then(res => res.json())
-            .then(json => setGenre(json))
-    }, [GENRE_URL]);
+  useEffect(() => {
+    fetch(GENRE_URL)
+      .then(res => res.json())
+      .then(json => setGenre(json));
+  }, [GENRE_URL]);
 
-    // Check if things are not okay
-    if (!genre) {
-        history.pushState('/games');
-    }
+  // Check if things are not okay
+  if (!genre) {
+    history.pushState("/games");
+  }
 
-    // Do things that are okay
-    return (
-        <section className="game-details">
-            <h2>{genre.name}</h2>
-            <section className="game-overview">
-                <img src={genre.image_background} alt={genre.name} />
-                <section className="game-stats">
-                    <h3>Games Count: {genre.games_count}</h3>
-                    <p>Description: {unescape(genre.description)}</p>
-                </section>
-            </section>
+  // Do things that are okay
+  return (
+    <section className="game-details">
+      <h2>{genre.name}</h2>
+      <section className="game-overview">
+        <img src={genre.image_background} alt={genre.name} />
+        <section className="game-stats">
+          <h3>Games Count: {genre.games_count}</h3>
+          <p>Description: {unescape(genre.description)}</p>
         </section>
-    )
-}
+      </section>
+    </section>
+  );
+};
