ee84223840133ea0a7f9a22ace557997c30fd9eb
Van <vango85@gmail.com>
2020-11-02 10:21:47 +0100

    [xx] Starting point for lecture 1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,5 @@
-import React from "react";
-import { GameList } from "./components/GameList";
-import { GamesDetails } from "./pages/GameDetails";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React from 'react';
 
 export const App = () => {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" exact>
-          <GameList />
-        </Route>
-        <Route path="/games/:slug" exact>
-          <GamesDetails />
-        </Route>
-      </Switch>
-    </BrowserRouter>
-  );
+  return <div />;
 };
